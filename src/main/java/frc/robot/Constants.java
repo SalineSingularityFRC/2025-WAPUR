4e30b5fc4f9d863e16a3704a2d614f6c7e0a3302
krazyness <brodyxdai@gmail.com>
2025-10-29 19:28:59 -0400

    Add configuration classes for double, int, and boolean types with Preferences integration

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,6 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Preferences;
 import edu.wpi.first.wpilibj.RobotBase;
 
 public class Constants {
@@ -14,5 +15,71 @@ public class Constants {
 
   public static enum Mode {
     REAL, SIM, REPLAY
+  }
+
+  public abstract static class Config<T> {
+    public final String name;
+    public final T defaultValue;
+
+    Config(String name, T defaultValue) {
+      this.name = name;
+      this.defaultValue = defaultValue;
+    }
+
+    abstract public T getValue();
+  }
+
+  public static class ConfigDouble extends Config<Double> {
+    public ConfigDouble(String name, double defaultValue) {
+      super(name, defaultValue);
+
+      // Make sure that it shows up in the Preferences
+      Preferences.initDouble(name, defaultValue);
+    }
+
+    @Override
+    public Double getValue() {
+      return Preferences.getDouble(name, defaultValue);
+    }
+  }
+
+  public static class ConfigInt extends Config<Integer> {
+    public ConfigInt(String name, int defaultValue) {
+      super(name, defaultValue);
+
+      // Make sure that it shows up in the Preferences
+      Preferences.initInt(name, defaultValue);
+    }
+
+    @Override
+    public Integer getValue() {
+      return Preferences.getInt(name, defaultValue);
+    }
+
+    public boolean isTrue() {
+      return getValue() != 0;
+    }
+
+    public boolean isFalse() {
+      return (!isTrue());
+    }
+  }
+
+  public static class ConfigBoolean extends Config<Boolean> {
+    public ConfigBoolean(String name, boolean defaultValue) {
+      super(name, defaultValue);
+
+      Preferences.initBoolean(name, defaultValue);
+    }
+
+    public Boolean getValue() {
+      return Preferences.getBoolean(name, defaultValue);
+    }
+  }
+
+  public static class Drive {
+    public static ConfigDouble HEADING_CORRECTION_KP = new ConfigDouble("Drive/PID/Heading Correction kP", 1);
+    public static ConfigDouble HEADING_CORRECTION_KI = new ConfigDouble("Drive/PID/Heading Correction kI", 0);
+    public static ConfigDouble HEADING_CORRECTION_KD = new ConfigDouble("Drive/PID/Heading Correction kD", 0);
   }
 }
