b80da07cadffb5f9bf72585a6a436796a94e426a
Fionn Hartley-Beane <hartleyk@salinehornets.org>
2025-12-03 20:13:04 -0500

    Added ElevatorSubsystem and IntakeSubsystem

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,6 +13,7 @@ import com.ctre.phoenix6.swerve.SwerveRequest;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.simulation.ElevatorSim;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -22,13 +23,15 @@ import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
-import frc.robot.subsystems.TestShooterSubsytem;
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.ElevatorSubsystem;
 
 public class RobotContainer {
     private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
     private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second
                                                                                       // max angular velocity
-    private TestShooterSubsytem shooterSubsystem = new TestShooterSubsytem();
+    private IntakeSubsystem Intake = new IntakeSubsystem();
+    private ElevatorSubsystem Elevator = new ElevatorSubsystem();
     private Pigeon2 gyro = new Pigeon2(20);
 
     /* Setting up bindings for necessary control of the swerve drive platform */
--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -9,25 +9,25 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
-public class TestShooterSubsytem extends SubsystemBase{
-    public TalonFX motor1 = new TalonFX(Constants.CANID.ShooterSubsystem.MOTOR1);
-    public TalonFX motor2 = new TalonFX(Constants.CANID.ShooterSubsystem.MOTOR2);
+public class IntakeSubsystem extends SubsystemBase{
+    public TalonFX motor1 = new TalonFX(Constants.CANID.IntakeSubsystem.LINTAKEMOTOR);
+    public TalonFX motor2 = new TalonFX(Constants.CANID.IntakeSubsystem.RINTAKEMOTOR);
     public TalonFXConfiguration motor1Config;
     public TalonFXConfiguration motor2Config;
 
-    public TestShooterSubsytem() {
+    public IntakeSubsystem() {
         motor1Config = new TalonFXConfiguration();
         motor2Config = new TalonFXConfiguration();
 
         motor1Config.CurrentLimits = new CurrentLimitsConfigs().withStatorCurrentLimit(4).withStatorCurrentLimitEnable(false);
-        motor1Config.Slot0.kP = Constants.PID.Shooter.kP.getValue();
-        motor1Config.Slot0.kI = Constants.PID.Shooter.kI.getValue();
-        motor1Config.Slot0.kD = Constants.PID.Shooter.kD.getValue();
+        motor1Config.Slot0.kP = Constants.PID.Intake.kP.getValue();
+        motor1Config.Slot0.kI = Constants.PID.Intake.kI.getValue();
+        motor1Config.Slot0.kD = Constants.PID.Intake.kD.getValue();
 
         motor2Config.CurrentLimits = new CurrentLimitsConfigs().withStatorCurrentLimit(4).withStatorCurrentLimitEnable(false);
-        motor2Config.Slot0.kP = Constants.PID.Shooter.kP.getValue();
-        motor2Config.Slot0.kI = Constants.PID.Shooter.kI.getValue();
-        motor2Config.Slot0.kD = Constants.PID.Shooter.kD.getValue();
+        motor2Config.Slot0.kP = Constants.PID.Intake.kP.getValue();
+        motor2Config.Slot0.kI = Constants.PID.Intake.kI.getValue();
+        motor2Config.Slot0.kD = Constants.PID.Intake.kD.getValue();
 
         motor1.getConfigurator().apply(motor1Config);
         motor2.getConfigurator().apply(motor2Config);
