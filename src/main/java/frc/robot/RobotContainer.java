3ddacc5b5aa57c16b45efdca6bf0c70a433cb644
Fionn Hartley-Beane <hartleyk@salinehornets.org>
2025-12-03 20:21:46 -0500

    Fixed Errors in ElevatorSubsystem

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -90,7 +90,6 @@ public class RobotContainer {
         // point.withModuleDirection(new Rotation2d(-joystick.getLeftY(),
         // -joystick.getLeftX()))
         // ));
-        joystick.a().whileTrue(shooterSubsystem.RunMotors(100));
 
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
